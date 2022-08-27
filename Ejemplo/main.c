ff9b9170c5a32b50bf62d537c35c621aee996836
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-08-27 01:47:03 -0500

    Operaciones con enteros, dentro de printf

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hola, mundo!\n");
+
+    printf("%d\n",2+3);
+    printf("%d\n",2-3);
+    printf("%d\n",2*3);
+    printf("%d\n",2/3);
+    printf("%d\n\n",2%3);
+
+    printf("2\t+\t3\t=\t%d\n",2+3);
+    printf("2\t-\t3\t=\t%d\n",2-3);
+    printf("2\t*\t3\t=\t%d\n",2*3);
+    printf("2\t/\t3\t=\t%d\n",2/3);
+    printf("2\t%%\t3\t=\t%d\n\n",2%3);
+
+    printf("%d\t+\t%d\t=\t%d\n",2,3,2+3);
+    printf("%d\t-\t%d\t=\t%d\n",2,3,2-3);
+    printf("%d\t*\t%d\t=\t%d\n",2,3,2*3);
+    printf("%d\t/\t%d\t=\t%d\n",2,3,2/3);
+    printf("%d\t%%\t%d\t=\t%d\n\n",2,3,2%3);
+
     return 0;
 }
