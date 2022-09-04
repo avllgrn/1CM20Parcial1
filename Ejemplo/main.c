7c51bc521e6eb21682bf857f6cb0cbe4645fd405
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-09-04 14:36:06 -0500

    Formula general

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -2,17 +2,27 @@
 #include <math.h>
 
 int main(void){
-    float g,r;
+    float a;
+    float b;
+    float c;
+    float x1;
+    float x2;
 
     //1. Pide datos
-    printf("Dame r ");
-    scanf("%f",&r);
+    printf("Dame a ");
+    scanf("%f",&a);
+    printf("Dame b ");
+    scanf("%f",&b);
+    printf("Dame c ");
+    scanf("%f",&c);
 
     //2. Calcula formula(s)
-    g = 180.0*r/M_PI;
+    x1 = (-b+sqrt(pow(b,2)-4*a*c))/(2*a);
+    x2 = (-b-sqrt(pow(b,2)-4*a*c))/(2*a);
 
     //3. Muestra resultado(s)
-    printf("%f rads = %f grads\n",r,g);
+    printf("x1 = %f\n",x1);
+    printf("x2 = %f\n",x2);
 
     return 0;
 }
