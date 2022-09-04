546fddacccb2e2915c6a20ed94098745cc2497d5
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-09-04 14:39:09 -0500

    Lbs a Kg

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -6,15 +6,14 @@ int main(void){
     float lbs;
 
     //1. Pide datos
-    printf("Dame kg ");
-    scanf("%f",&kg);
+    printf("Dame lbs ");
+    scanf("%f",&lbs);
  
     //2. Calcula formula(s)
-    lbs = kg * 2.20462;
+    kg = lbs / 2.20462;
  
-
     //3. Muestra resultado(s)
-    printf("%f kg = %flbs\n",kg,lbs);
+    printf("%f lbs = %fkg\n",lbs,kg);
 
     return 0;
 }
