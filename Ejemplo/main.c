180e3d598697b5c1dd787e765c8e52bb74f7c596
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-09-04 14:37:43 -0500

    Kg a Lbs

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -2,27 +2,19 @@
 #include <math.h>
 
 int main(void){
-    float a;
-    float b;
-    float c;
-    float x1;
-    float x2;
+    float kg;
+    float lbs;
 
     //1. Pide datos
-    printf("Dame a ");
-    scanf("%f",&a);
-    printf("Dame b ");
-    scanf("%f",&b);
-    printf("Dame c ");
-    scanf("%f",&c);
-
+    printf("Dame kg ");
+    scanf("%f",&kg);
+ 
     //2. Calcula formula(s)
-    x1 = (-b+sqrt(pow(b,2)-4*a*c))/(2*a);
-    x2 = (-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+    lbs = kg * 2.20462;
+ 
 
     //3. Muestra resultado(s)
-    printf("x1 = %f\n",x1);
-    printf("x2 = %f\n",x2);
+    printf("%f kg = %flbs\n",kg,lbs);
 
     return 0;
 }
