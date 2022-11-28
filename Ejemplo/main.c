babef7cc66fce6a54dd7ef6974b3b5f845247e6e
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-11-27 18:14:56 -0600

    Permitir acceso a mayores de edad (version 1)

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
 
-//Declaracion de Funcion vacia (sin retorno ni argumento(s))
-void suma(void);
-
 int main(void){
+    int edad;
 
-    //Invocacion de Funcion vacia (sin retorno ni argumento(s))
-    suma();
+    printf("Ingresa tu edad ");
+    scanf("%d",&edad);
+
+    if(edad >= 18){
+        printf("Pase, Ud.! =^)\n\n");
+    }
 
     return 0;
 }
-
-//Definicion de Funcion vacia (sin retorno ni argumento(s))
-void suma(void){
-    //La funcion hace 1, 2 y 3
-    int a, b, c;
-
-    //1. Pide dato(s)
-    printf("Dame un numero ");
-    scanf("%d",&a);
-    printf("Dame otro numero ");
-    scanf("%d",&b);
-
-    //2. Calcula operacion(es)
-    c=a+b;
-
-    //3.Muestra resultado(s), c
-    printf("%d + %d = %d\n",a,b,c);
-}
-
