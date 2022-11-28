6f8f4e1875e2bc63a4850520d7f564ca163f1081
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-11-27 18:16:34 -0600

    Prohibir acceso a menores de edad (version 1)

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -6,8 +6,8 @@ int main(void){
     printf("Ingresa tu edad ");
     scanf("%d",&edad);
 
-    if(edad > 17){
-        printf("Pase, Ud.! =^)\n\n");
+    if(edad < 18){
+        printf("No puede pasar Ud.! =^)\n\n");
     }
 
     return 0;
