0e42f23a6cbd94095804cb0d74b419277ca4bda7
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-11-29 13:33:17 -0600

    if-else Edad (Dos segmentos)

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -6,8 +6,11 @@ int main(void){
     printf("Ingresa tu edad ");
     scanf("%d",&edad);
 
-    if(edad <= 17){
-        printf("No puede pasar Ud.! =^)\n\n");
+    if(edad > 17){
+        printf("Pase Ud.! =^)\n\n");
+    }
+    else{
+        printf("NO puede pasar Ud.! =^)\n\n");
     }
 
     return 0;
