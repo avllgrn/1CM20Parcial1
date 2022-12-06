ecbba550780265058d1a84c12f45275f19f645aa
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-06 12:07:46 -0600

    Funcion esLetra()

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+int esCaracterMayuscula(char caracter);
 int esCaracterMinuscula(char caracter);
+int esLetra(char caracter);
 
 int main(void){
     char caracter;
@@ -8,7 +10,7 @@ int main(void){
     printf("Ingresa un caracter ");
     scanf("%c",&caracter);
 
-    if(esCaracterMinuscula(caracter)){
+    if(esLetra(caracter)){
         printf("Si es.\n\n");
     }
     else{
@@ -17,12 +19,38 @@ int main(void){
 
     return 0;
 }
+int esCaracterMayuscula(char caracter){
+    if(caracter < 65){
+        return 0;
+    }
+    else{
+        if(caracter <= 90){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
 int esCaracterMinuscula(char caracter){
     if(caracter < 97){
         return 0;
     }
     else{
         if(caracter <= 122){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+int esLetra(char caracter){
+    if(esCaracterMayuscula(caracter)){
+        return 1;
+    }
+    else{
+        if(esCaracterMinuscula(caracter)){
             return 1;
         }
         else{
