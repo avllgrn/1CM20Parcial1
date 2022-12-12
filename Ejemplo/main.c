2c4b9e01ac446f9b6b642fcb52dc7b4e0afb9cc4
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-12 16:30:34 -0600

    Genero con if-else dentro de if-else

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,88 +1,29 @@
 #include <stdio.h>
-
-int esCaracterNumerico(char caracter);
-int esCaracterMayuscula(char caracter);
-int esCaracterMinuscula(char caracter);
-int esLetra(char caracter);
-int esCaracterEspecial(char caracter);
-
 int main(void){
-    char caracter;
+    char genero;
+    printf("Ingresa un genero ");
+    scanf("%c",&genero);
 
-    printf("Ingresa un caracter ");
-    scanf("%c",&caracter);
-
-    if(esCaracterEspecial(caracter)){
-        printf("Si es.\n\n");
+    if(genero == 'f'){
+        printf("femenino\n");
     }
     else{
-        printf("No es.\n\n");
+        if(genero == 'F'){
+            printf("FEMENINO\n");
+        }
+        else{
+            if(genero == 'm'){
+                printf("mascuilino\n");
+            }
+            else{
+                if(genero == 'M'){
+                    printf("MASCULINO\n");
+                }
+                else{
+                    printf("NO reconocido\n");
+                }
+            }
+        }
     }
-
     return 0;
-}
-int esCaracterNumerico(char caracter){
-    if(caracter < 48){
-        return 0;
-    }
-    else{
-        if(caracter <= 57){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-}
-int esCaracterMayuscula(char caracter){
-    if(caracter < 65){
-        return 0;
-    }
-    else{
-        if(caracter <= 90){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-}
-int esCaracterMinuscula(char caracter){
-    if(caracter < 97){
-        return 0;
-    }
-    else{
-        if(caracter <= 122){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-}
-int esLetra(char caracter){
-    if(esCaracterMayuscula(caracter)){
-        return 1;
-    }
-    else{
-        if(esCaracterMinuscula(caracter)){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-}
-int esCaracterEspecial(char caracter){
-    if(esLetra(caracter)){
-        return 0;
-    }
-    else{
-        if(esCaracterNumerico(caracter)){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    }
 }
