f133259dd7f2a05e28f5046f7313bcd4f9e08dcb
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-15 13:31:10 -0600

    Genero switch

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,60 +1,22 @@
 #include <stdio.h>
 
 int main(void){
-    int genero, estadoCivil;
-    printf("Ingresa tu genero y estado civil (juntos sin espacios) ");
-    scanf("%c%c",&genero,&estadoCivil);
+    int genero;
+    printf("Ingresa tu genero ");
+    scanf("%c",&genero);
 
-    if(genero=='f' || genero=='F'){
-        if(estadoCivil=='s' || estadoCivil=='S'){
-            printf("Soltera");
-        }
-        else if(estadoCivil=='c' || estadoCivil=='C'){
-            printf("Casada");
-        }
-        else if(estadoCivil=='d' || estadoCivil=='D'){
-            printf("Divorciada");
-        }
-        else if(estadoCivil=='v' || estadoCivil=='V'){
-            printf("Viuda");
-        }
-        else{
-            printf("Femenino, EC: NO reconocido");
-        }
-    }
-    else if(genero=='m' || genero=='M'){
-        if(estadoCivil=='s' || estadoCivil=='S'){
-            printf("Soltero");
-        }
-        else if(estadoCivil=='c' || estadoCivil=='C'){
-            printf("Casado");
-        }
-        else if(estadoCivil=='d' || estadoCivil=='D'){
-            printf("Divorciado");
-        }
-        else if(estadoCivil=='v' || estadoCivil=='V'){
-            printf("Viudo");
-        }
-        else{
-            printf("Masculino, EC: NO reconocido");
-        }
-    }
-    else{
-        if(estadoCivil=='s' || estadoCivil=='S'){
-            printf("Genero: NO reconocido, EC: Soltero");
-        }
-        else if(estadoCivil=='c' || estadoCivil=='C'){
-            printf("Genero: NO reconocido, EC: Casado");
-        }
-        else if(estadoCivil=='d' || estadoCivil=='D'){
-            printf("Genero: NO reconocido, EC: Divorciado");
-        }
-        else if(estadoCivil=='v' || estadoCivil=='V'){
-            printf("Genero: NO reconocido, EC: Viudo");
-        }
-        else{
-            printf("Genero: NO reconocido, EC: NO reconocido");
-        }
+    switch(genero){
+        case 'f':
+        case 'F':
+            printf("Femenino");
+            break;
+        case 'm':
+        case 'M':
+            printf("Masculino");
+            break;
+        default:
+            printf("NR");
+            break;
     }
 
     return 0;
