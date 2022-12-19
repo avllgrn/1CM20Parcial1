f19f82f94411ecdd968acbff4b19b20c0ae5e561
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-19 15:05:20 -0600

    Ejemplos de un menu de 5 opciones con switch

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,83 +1,77 @@
 #include <stdio.h>
+#include <math.h>
+
+void esPar(void){
+    int x;
+    printf("Dame un numero ");scanf("%d",&x);
+    if(x%2==0){
+        printf("Es par\n");
+    }
+    else{
+        printf("NO es par\n");
+    }
+}
+void esMultiploDe7(void){
+    int x;
+    printf("Dame un numero ");scanf("%d",&x);
+    if(x%7==0){
+        printf("Es multiplo de 7\n");
+    }
+    else{
+        printf("NO es multiplo de 7\n");
+    }
+}
+void sumaDosNumeros(void){
+    int a, b, c;
+    printf("Dame un numero ");scanf("%d",&a);
+    printf("Dame otro numero ");scanf("%d",&b);
+    c=a+b;
+    printf("%d + %d = %d",a,b,c);
+}
+void raicesDeEcuacionCuadratica(void){
+    float a, b, c, x1, x2;
+    printf("Dame a ");scanf("%f",&a);
+    printf("Dame b ");scanf("%f",&b);
+    printf("Dame c ");scanf("%f",&c);
+    if(a==0){
+        printf("Error! Indeterminacion... =(\n\n");
+    }
+    else if(pow(b,2)-4*a*c < 0){
+        printf("Error! Raices imaginarias... =(\n\n");
+    }
+    else{
+        x1 = (-b+sqrt(pow(b,2)-4*a*c))/(2*a);
+        x2 = (-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+        printf("x1 = %f\n",x1);
+        printf("x2 = %f\n",x2);
+    }
+}
 
 int main(void){
-    char genero, estadoCivil;
-    printf("Ingresa tu genero y Estado civil ");
-    scanf("%c%c",&genero,&estadoCivil);
-
-    switch(genero){
-        case 'f':
-        case 'F':
-            switch(estadoCivil){
-                case 's':
-                case 'S':
-                    printf("Soltera");
-                    break;
-                case 'c':
-                case 'C':
-                    printf("Casada");
-                    break;
-                case 'd':
-                case 'D':
-                    printf("Divorciada");
-                    break;
-                case 'v':
-                case 'V':
-                    printf("Viuda");
-                    break;
-                default:
-                    printf("F, EC: NR");
-                    break;
-            }
+    int opcion;
+    printf("1. Es par\n");
+    printf("2. Es multiplo de 7\n");
+    printf("3. Suma numeros\n");
+    printf("4. Formula General\n");
+    printf("5. Salir\n");
+    printf("Cual es tu opcio? ");
+    scanf("%d",&opcion);
+    switch(opcion){
+        case 1:esPar();
             break;
-        case 'm':
-        case 'M':
-            switch(estadoCivil){
-                case 's':
-                case 'S':
-                    printf("Soltero");
-                    break;
-                case 'c':
-                case 'C':
-                    printf("Casado");
-                    break;
-                case 'd':
-                case 'D':
-                    printf("Divorciado");
-                    break;
-                case 'v':
-                case 'V':
-                    printf("Viudo");
-                    break;
-                default:
-                    printf("M, EC: NR");
-                    break;
-            }
+        case 2:esMultiploDe7();
+            break;
+        case 3:sumaDosNumeros();
+            break;
+        case 4:
+            raicesDeEcuacionCuadratica();
+            break;
+        case 5:
+            printf("Adios!\n");
             break;
         default:
-            switch(estadoCivil){
-                case 's':
-                case 'S':
-                    printf("Genero: NR, Soltero");
-                    break;
-                case 'c':
-                case 'C':
-                    printf("Genero: NR, Casado");
-                    break;
-                case 'd':
-                case 'D':
-                    printf("Genero: NR, Divorciado");
-                    break;
-                case 'v':
-                case 'V':
-                    printf("Genero: NR, Viudo");
-                    break;
-                default:
-                    printf("Genero: NR, EC: NR");
-                    break;
-            }
+            printf("Opcion invalida!\n");
             break;
     }
-
     return 0;
 }
