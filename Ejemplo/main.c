ba84c1824fd56d9d3b442efd661da512297e7de2
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 17:14:37 -0600

    Contador de 10 a 0, de 1 en 1, con do-while

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -6,10 +6,10 @@ int main(void){
     int i;//Variable contador
 
     i = 10;//Variable que comienza en un numero
-    for( ; i>0 ; ){//Acaba en otro
+    do{
         printf("%d ",i);//Se muestra cada valor que toma el contador
         i = i - 1;//Retrocede de uno en uno
-    }
+    }while( i>0 );//Acaba en otro
 
     return 0;
 }
