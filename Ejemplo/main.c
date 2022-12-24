22c0b2aac05895fef2c333a6dc342ece0e500041
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 17:07:58 -0600

    Contador descendente con do-while

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -6,10 +6,10 @@ int main(void){
     int i;//Variable contador
 
     i = 0;//Variable que comienza en un numero
-    for( ; 1 ; ){//Acaba en otro (en el infinito)
+    do{//Acaba en otro (en el infinito)
         printf("%d ",i);//Se muestra cada valor que toma el contador
         i = i - 1;//Retrocede de uno en uno
-    }
+    }while( 1 );
 
     return 0;
 }
