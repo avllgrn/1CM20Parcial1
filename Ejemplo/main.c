e8357510a3dc261ed42d61019cc76955800c2a23
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 17:12:26 -0600

    Contador de 0 a 10, de 1 en 1, con for

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -6,7 +6,7 @@ int main(void){
     int i;//Variable contador
 
     i = 0;//Variable que comienza en un numero
-    while( i<10 ){//Acaba en otro
+    for( ; i<10 ; ){//Acaba en otro
         printf("%d ",i);//Se muestra cada valor que toma el contador
         i = i + 1;//Avanza de uno en uno
     }
