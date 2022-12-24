684bc9b4554c025ec67b1e3b3b2e37066dbe1396
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 17:04:46 -0600

    Contador de 1 a infinito de uno en uno, con do-while (no se ve)

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -6,9 +6,9 @@ int main(void){
     int i;//Variable contador
 
     i = 1;//Variable que comienza en un numero
-    for( ; 1 ; ){//Acaba en otro (en el infinito)
+    do{
         i = i + 1;//Avanza de uno en uno
-    }
+    }while( 1 );//Acaba en otro (en el infinito)
 
     return 0;
 }
