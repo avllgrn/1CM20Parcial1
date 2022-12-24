40182f48ea7667695f4b6f7a222f8efde58e4452
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 17:07:20 -0600

    Contador descendente con for

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -6,7 +6,7 @@ int main(void){
     int i;//Variable contador
 
     i = 0;//Variable que comienza en un numero
-    while( 1 ){//Acaba en otro (en el infinito)
+    for( ; 1 ; ){//Acaba en otro (en el infinito)
         printf("%d ",i);//Se muestra cada valor que toma el contador
         i = i - 1;//Retrocede de uno en uno
     }
