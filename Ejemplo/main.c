4e2aebbf3043c0c9da17d883888a28633b963f5b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 22:24:20 -0600

    Mensaje en otro ciclo infinito, con for (y variables)

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -5,7 +5,7 @@ int main(void){
     int b=3;
     //Conjunto de instrucciones que se repite mientras a<b sea verdadero (siempre para 2 y 3)
     //Mensaje en un ciclo infinto
-    while( a<b ){
+    for( ; a<b ; ){
         printf("Hola, mundo! ");
     }
     return 0;
