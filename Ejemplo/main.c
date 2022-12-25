c66ab6593efdd57b3ce968b9c33fd7bddad8e30c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 22:11:27 -0600

    Otro ciclo infinito de conjunto de instrucciones vacias, con do-while

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -3,8 +3,8 @@
 int main(void){
     //Conjunto de instrucciones que se repite mientras 2<3 sea verdadero (siempre)
     //Ciclo infinto de conjunto vacio de instrucciones
-    for( ; 2<3 ; ){
+    do{
 
-    }
+    }while( 2<3 );
     return 0;
 }
