1b33681766ba4bac8b364c0660a5d7e2918e7b11
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 22:10:12 -0600

    Otro ciclo infinito de conjunto de instrucciones vacias, con while

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    //Conjunto de instrucciones que se repite mientras 1 sea verdadero (siempre)
+    //Conjunto de instrucciones que se repite mientras 2<3 sea verdadero (siempre)
     //Ciclo infinto de conjunto vacio de instrucciones
-    do{
+    while( 2<3 ){
 
-    }while( 1 );
+    }
     return 0;
 }
