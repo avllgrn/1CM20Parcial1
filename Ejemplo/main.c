efd28545a419bb287b32f8100fd21fe57592d167
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 22:21:46 -0600

    Mensaje en otro ciclo infinito, con while

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    //Conjunto de instrucciones que se repite mientras 1 sea verdadero (siempre)
+    //Conjunto de instrucciones que se repite mientras 2<3 sea verdadero (siempre)
     //Mensaje en un ciclo infinto
-    do{
+    while( 2<3 ){
         printf("Hola, mundo! ");
-    }while( 1 );
+    }
     return 0;
 }
