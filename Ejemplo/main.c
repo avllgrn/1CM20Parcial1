7482c477ca110415eee1b718c6ba5b664003dc2b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 22:33:30 -0600

    Menu en un ciclo infinito, con while

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,12 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void esPar(void);
+void esMultiploDe7(void);
+void sumaDosNumeros(void);
+void raicesDeEcuacionCuadratica(void);
 
 int main(void){
-    int a=2;
-    int b=3;
-    //Conjunto de instrucciones que se repite mientras a<b sea verdadero (siempre para 2 y 3)
-    //Mensaje en un ciclo infinto
-    do{
-        printf("Hola, mundo! ");
-    }while( a<b );
+    int opcion=0;//Se fuerza al while a ejecutar al conjunto de instrucciones, al menos una vez
+
+    //Conjunto de instrucciones que se repite mientras la opcion no sea 5
+    //Menu en un ciclo que se repite mientras opcion no sea 5
+    while(opcion != 5){
+        system("cls");
+        printf("1. Es par\n");
+        printf("2. Es multiplo de 7\n");
+        printf("3. Suma numeros\n");
+        printf("4. Formula General\n");
+        printf("5. Salir\n");
+        printf("Cual es tu opcio? ");
+        scanf("%d",&opcion);
+        system("cls");
+        switch(opcion){
+            case 1:esPar();
+                break;
+            case 2:esMultiploDe7();
+                break;
+            case 3:sumaDosNumeros();
+                break;
+            case 4:
+                raicesDeEcuacionCuadratica();
+                break;
+            case 5:
+                printf("Adios!\n\n");
+                break;
+            default:
+                printf("Opcion invalida!\n\n");
+                break;
+        }
+        system("pause");
+    }
     return 0;
+}
+
+void esPar(void){
+    int x;
+    printf("Dame un numero ");scanf("%d",&x);
+    if(x%2==0){
+        printf("Es par\n\n");
+    }
+    else{
+        printf("NO es par\n\n");
+    }
+}
+void esMultiploDe7(void){
+    int x;
+    printf("Dame un numero ");scanf("%d",&x);
+    if(x%7==0){
+        printf("Es multiplo de 7\n\n");
+    }
+    else{
+        printf("NO es multiplo de 7\n\n");
+    }
+}
+void sumaDosNumeros(void){
+    int a, b, c;
+    printf("Dame un numero ");scanf("%d",&a);
+    printf("Dame otro numero ");scanf("%d",&b);
+    c=a+b;
+    printf("%d + %d = %d\n\n",a,b,c);
+}
+void raicesDeEcuacionCuadratica(void){
+    float a, b, c, x1, x2;
+    printf("Dame a ");scanf("%f",&a);
+    printf("Dame b ");scanf("%f",&b);
+    printf("Dame c ");scanf("%f",&c);
+    if(a==0){
+        printf("Error! Indeterminacion... =(\n\n");
+    }
+    else if(pow(b,2)-4*a*c < 0){
+        printf("Error! Raices imaginarias... =(\n\n");
+    }
+    else{
+        x1 = (-b+sqrt(pow(b,2)-4*a*c))/(2*a);
+        x2 = (-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+        printf("x1 = %f\n",x1);
+        printf("x2 = %f\n\n",x2);
+    }
 }
