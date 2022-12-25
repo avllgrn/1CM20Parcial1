f7968670082a5a9e7bed7a84ccd979c1f722c864
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 22:19:34 -0600

    Mensaje en ciclo infinito, con do-while

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -3,8 +3,8 @@
 int main(void){
     //Conjunto de instrucciones que se repite mientras 1 sea verdadero (siempre)
     //Mensaje en un ciclo infinto
-    for( ; 1 ; ){
+    do{
         printf("Hola, mundo! ");
-    }
+    }while( 1 );
     return 0;
 }
