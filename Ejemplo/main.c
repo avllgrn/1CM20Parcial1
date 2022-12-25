1954dca29d29fced20cadb500a581b44c1182c54
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-12-24 22:19:07 -0600

    Mensaje en ciclo infinito, con for

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -3,7 +3,7 @@
 int main(void){
     //Conjunto de instrucciones que se repite mientras 1 sea verdadero (siempre)
     //Mensaje en un ciclo infinto
-    while( 1 ){
+    for( ; 1 ; ){
         printf("Hola, mundo! ");
     }
     return 0;
